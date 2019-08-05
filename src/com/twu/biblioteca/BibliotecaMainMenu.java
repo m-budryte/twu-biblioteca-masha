196b30218338bc9667308222674095a4bdad1fa8
Masha <mashabudryte@gmail.com>
2019-08-05 13:45:45 +0100

    refactoring in the Main menu to allow for future menues

--- src/com/twu/biblioteca/BibliotecaMainMenu.java
+++ src/com/twu/biblioteca/BibliotecaMainMenu.java
@@ -1,12 +1,23 @@
 package com.twu.biblioteca;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class BibliotecaMainMenu {
+    String menu;
+    String [] validOptions;
+
+    public BibliotecaMainMenu(){
+        menu = "9. Exit";
+        validOptions = new String[]{"9"};
+    }
     public String showMenu() {
-        return "9. Exit";
+        return menu;
     }
 
     public String chooseOption(String s) {
-        if (s == "9") {
+        List<String> list = Arrays.asList(validOptions);
+        if (list.contains(s)) {
             return "Valid option";
         } else {
             return "Invalid option. Please try again.";
